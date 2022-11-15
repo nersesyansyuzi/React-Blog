9130afdd663759e6f46b4708ef8ee1edda438d36
Syuzanna <nersisyansuzi@gmail.com>
2022-11-15 23:31:39 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 
 import './App.css';
 import Home from "./Componenst/Home/Home"
-import { useContext, useState } from "react";
 import Menu from "./Componenst/Menu/Menu"
-import { MyContext } from "./Componenst/Context/Context"
 import CreateBlog from "./Componenst/Create/CreateBlog"
 import { Route, Routes } from "react-router-dom"
 import Layout from './Componenst/Layout';
 import BlogDetail from './Componenst/Detail/BlogDetail';
 
 function App() {
-  const { blog: { showMenu }, dispatch } = useContext(MyContext)
   return (
     <>
       <Routes>
